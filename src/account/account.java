685fb9d8fa4f121ba838ba231adb9872e6ca5b58
rafaScalet <rafaelscalet2018@gmail.com>
2024-04-29 18:24:29 -0300

    created variables and functions

--- src/account/account.java
+++ src/account/account.java
@@ -1,5 +1,30 @@
 package account;
 
 public class account {
-    
+    public int id;
+    public double balance;
+    public double cap;
+    public String name;
+
+    public boolean pop(double value) {
+        if (this.balance >= value && value <= this.cap) {
+            this.balance -= value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double deposit(double value) {
+        return this.balance += value;
+    }
+
+    public boolean transfer(account destiny, double value) {
+        if (this.cap >= value) {
+            this.balance -= value;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,18 @@
 package main;
 
+import account.account;
+
 public class Main {
     public static void main(String[] args){
-        
+        account acc = new account();
+
+        acc.id = 1;
+        acc.name = "nome";
+        acc.balance = 10000;
+        acc.cap = 100;
+
+        System.out.println(acc.pop(100) ? "o saque foi efetuado, valor da conta: " + acc.balance : "erro ao realizar o saque");
+        System.out.println("Depósito: " + acc.deposit(100));
+        System.out.println(acc.transfer(acc, 100) ? "a transferência foi efetuada com sucesso" : "a transferência não foi concluída");
     }
 }
